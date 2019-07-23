ec50a7607795aab6c0415591f726e6b330cb0622
Raghavendra V <http://localhost:9000>
2019-07-23 09:52:46 +0530

    port changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use('/todo', todoRoutes);
 app.use('/reminder', reminderRoutes);
 
 //PORT 
-const PORT = process.env.PORT || 3000
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
